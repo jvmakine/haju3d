f20972c54f69ec891fd3ec2ab45fe736dac8c884
jvmakine <jvmakine@gmail.com>
2013-08-10 00:23:44 +0300

    Added client runner and uber-jar packaging to run it

---
+++ src/main/java/fi/haju/haju3d/client/ClientRunner.java
@@ -0,0 +1,12 @@
+package fi.haju.haju3d.client;
+
+/**
+ * Class to start the client
+ */
+public class ClientRunner {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
