1e830ecfffbf1ecb3485bed3d79a989b74b342f8
jvmakine <jvmakine@gmail.com>
2013-08-13 23:27:13 +0300

    Initial world structure classes

---
+++ src/main/java/fi/haju/haju3d/client/world/Chunk.java
@@ -0,0 +1,36 @@
+package fi.haju.haju3d.client.world;
+
+import java.io.Serializable;
+
+/**
+ * 64 x 64 x 64 set of blocks
+ */
+public abstract class Chunk implements Serializable {
+  
+  private static final long serialVersionUID = 1L;
+  
+  private int x;
+  private int y;
+  private int z;
+  
+  public abstract Block getBlockAt(int x, int y, int z);
+  
+  public Chunk(int x, int y, int z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  public int getXCoord() {
+    return x;
+  }
+  
+  public int getYCoord() {
+    return y;
+  }
+  
+  public int getZCoord() {
+    return z;
+  }
+  
+}
---
+++ src/main/java/fi/haju/haju3d/client/world/Material.java
@@ -0,0 +1,7 @@
+package fi.haju.haju3d.client.world;
+
+import java.io.Serializable;
+
+public interface Material extends Serializable {
+
+}
