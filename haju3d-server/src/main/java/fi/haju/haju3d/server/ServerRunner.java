60df5148c038a97f99f048861d2ab2ae34821118
jvmakine <jvmakine@gmail.com>
2013-09-24 21:40:02 +0300

    Improved server logging

--- haju3d-server/src/main/java/fi/haju/haju3d/server/ServerRunner.java
+++ haju3d-server/src/main/java/fi/haju/haju3d/server/ServerRunner.java
@@ -5,23 +5,29 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import fi.haju.haju3d.protocol.Server;
 
 public class ServerRunner {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(ServerRunner.class);
+  private static final String SERVER_NAME = "haju3d_server";
+  private static final int PORT = 5250;
+  
   public static void main(String[] args) {
     try {
-      Registry registry = LocateRegistry.createRegistry(5250);
+      Registry registry = LocateRegistry.createRegistry(PORT);
       ServerImpl server = new ServerImpl();
-      Server stub = (Server)UnicastRemoteObject.exportObject(server, 5250);
-      registry.rebind("haju3d_server", stub);
-      System.out.println("Haju3d bound");
+      Server stub = (Server)UnicastRemoteObject.exportObject(server, PORT);
+      registry.rebind(SERVER_NAME, stub);
+      LOGGER.info("Started Haju3D server at " + PORT);
       while(true) {
         Thread.sleep(100);
       }
     } catch (RemoteException | InterruptedException e) {
-      System.err.println("Haju3d exception:");
-      e.printStackTrace();
+      LOGGER.error("Error running Haju3D server", e);
     }
   }
 
