6efc6aa6011db05ab38e23feb8aabfdfd36ad9eb
jvmakine <jvmakine@gmail.com>
2013-10-06 18:24:11 +0300

    Moved input bindings to separate class from ChunkRenderer

--- haju3d-client/src/main/java/fi/haju/haju3d/client/ui/ChunkRenderer.java
+++ haju3d-client/src/main/java/fi/haju/haju3d/client/ui/ChunkRenderer.java
@@ -1,19 +1,10 @@
 package fi.haju.haju3d.client.ui;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import com.jme3.app.SimpleApplication;
 import com.jme3.asset.plugins.ClasspathLocator;
 import com.jme3.bounding.BoundingSphere;
 import com.jme3.bounding.BoundingVolume;
 import com.jme3.collision.CollisionResults;
-import com.jme3.input.KeyInput;
-import com.jme3.input.MouseInput;
-import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.AnalogListener;
-import com.jme3.input.controls.KeyTrigger;
-import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
@@ -39,6 +30,7 @@ import fi.haju.haju3d.client.Character;
 import fi.haju.haju3d.client.ChunkProvider;
 import fi.haju.haju3d.client.CloseEventHandler;
 import fi.haju.haju3d.client.ui.input.InputActions;
+import fi.haju.haju3d.client.ui.input.WorldInputHandler;
 import fi.haju.haju3d.client.ui.mesh.ChunkSpatialBuilder;
 import fi.haju.haju3d.protocol.Vector3i;
 
@@ -46,10 +38,8 @@ import fi.haju.haju3d.protocol.Vector3i;
  * Renderer application for rendering chunks from the server
  */
 public class ChunkRenderer extends SimpleApplication {
+  
   private static final float MOVE_SPEED = 40;
-  private static final float MOUSE_X_SPEED = 3.0f;
-  private static final float MOUSE_Y_SPEED = MOUSE_X_SPEED;
-
   
   private static final int CHUNK_CUT_OFF = 3;
   private static final Vector3f lightDir = new Vector3f(-0.9140114f, 0.29160172f, -0.2820493f).negate();
@@ -62,7 +52,7 @@ public class ChunkRenderer extends SimpleApplication {
   private boolean isFullScreen = false;
   private Node terrainNode = new Node("terrain");
   private WorldManager worldManager;
-  private Set<String> activeInputs = new HashSet<>();
+  private WorldInputHandler inputHandler;
   
   private Character character;
   
@@ -88,13 +78,16 @@ public class ChunkRenderer extends SimpleApplication {
     this.builder = new ChunkSpatialBuilder(assetManager);
     this.worldManager = new WorldManager(chunkProvider, builder);
     this.worldManager.start();
-
+    
     setupInput();
     setupCamera();
     setupSky();
     setupLighting();
     setupCharacter();
     setupPostFilters();
+    
+    inputHandler = new WorldInputHandler(character, worldManager, this);
+    inputHandler.register(inputManager);
 
     rootNode.attachChild(terrainNode);
   }
@@ -120,86 +113,12 @@ public class ChunkRenderer extends SimpleApplication {
 
   private void setupCamera() {
     getFlyByCamera().setMoveSpeed(MOVE_SPEED);
-    getFlyByCamera().setRotationSpeed(MOUSE_X_SPEED);
+    getFlyByCamera().setRotationSpeed(WorldInputHandler.MOUSE_X_SPEED);
     getCamera().setLocation(worldManager.getGlobalPosition(new Vector3i().add(32, 62, 62)));
   }
 
   private void setupInput() {
-    // moving
-    inputManager.addMapping(InputActions.STRAFE_LEFT, new KeyTrigger(KeyInput.KEY_A));
-    inputManager.addMapping(InputActions.STRAFE_RIGHT, new KeyTrigger(KeyInput.KEY_D));
-    inputManager.addMapping(InputActions.WALK_FORWARD, new KeyTrigger(KeyInput.KEY_W));
-    inputManager.addMapping(InputActions.WALK_BACKWARD, new KeyTrigger(KeyInput.KEY_S));
-    inputManager.addListener(new ActionListener() {
-      @Override
-      public void onAction(String name, boolean isPressed, float tpf) {
-        if (isPressed) {
-          activeInputs.add(name);
-        } else {
-          activeInputs.remove(name);
-        }
-      }
-    }, InputActions.STRAFE_LEFT, InputActions.STRAFE_RIGHT, InputActions.WALK_FORWARD, InputActions.WALK_BACKWARD);
-
-    // looking
-    inputManager.addMapping(InputActions.LOOK_LEFT, new MouseAxisTrigger(MouseInput.AXIS_X, true), new KeyTrigger(KeyInput.KEY_LEFT));
-    inputManager.addMapping(InputActions.LOOK_RIGHT, new MouseAxisTrigger(MouseInput.AXIS_X, false), new KeyTrigger(KeyInput.KEY_RIGHT));
-    inputManager.addMapping(InputActions.LOOK_UP, new MouseAxisTrigger(MouseInput.AXIS_Y, false), new KeyTrigger(KeyInput.KEY_UP));
-    inputManager.addMapping(InputActions.LOOK_DOWN, new MouseAxisTrigger(MouseInput.AXIS_Y, true), new KeyTrigger(KeyInput.KEY_DOWN));
-
-    inputManager.addListener(new AnalogListener() {
-      @Override
-      public void onAnalog(String name, float value, float tpf) {
-        if (name.equals(InputActions.LOOK_LEFT)) {
-          character.setLookAzimuth(character.getLookAzimuth() + value * MOUSE_X_SPEED);
-        } else if (name.equals(InputActions.LOOK_RIGHT)) {
-          character.setLookAzimuth(character.getLookAzimuth() - value * MOUSE_X_SPEED);
-        } else if (name.equals(InputActions.LOOK_UP)) {
-          character.setLookElevation(character.getLookElevation() - value * MOUSE_Y_SPEED);
-        } else if (name.equals(InputActions.LOOK_DOWN)) {
-          character.setLookElevation(character.getLookElevation() + value * MOUSE_Y_SPEED);
-        }
-      }
-    }, InputActions.LOOK_LEFT, InputActions.LOOK_RIGHT, InputActions.LOOK_UP, InputActions.LOOK_DOWN);
-
-    // jumping
-    inputManager.addMapping(InputActions.JUMP, new KeyTrigger(KeyInput.KEY_SPACE));
-    inputManager.addListener(new ActionListener() {
-      @Override
-      public void onAction(String name, boolean isPressed, float tpf) {
-        if (isPressed && canJump(character.getNode())) {
-          character.setVelocity(character.getVelocity().add(new Vector3f(0.0f, 0.5f, 0.0f)));
-        }
-      }
-    }, InputActions.JUMP);
-
-    // toggle flycam
-    inputManager.addMapping(InputActions.TOGGLE_FLYCAM, new KeyTrigger(KeyInput.KEY_RETURN));
-    inputManager.addListener(new ActionListener() {
-      @Override
-      public void onAction(String name, boolean isPressed, float tpf) {
-        if (isPressed) {
-          flyCam.setEnabled(!flyCam.isEnabled());
-          inputManager.setCursorVisible(false);
-        }
-      }
-    }, InputActions.TOGGLE_FLYCAM);
-
-    // toggle fullscreen
-    inputManager.addMapping(InputActions.CHANGE_FULL_SCREEN, new KeyTrigger(KeyInput.KEY_F));
-    inputManager.addListener(new ActionListener() {
-      @Override
-      public void onAction(String name, boolean keyPressed, float tpf) {
-        isFullScreen = !isFullScreen;
-        setDisplayMode();
-        restart();
-      }
-    }, InputActions.CHANGE_FULL_SCREEN);
-  }
-
-  private boolean canJump(Node characterNode) {
-    Vector3f pos = characterNode.getLocalTranslation();
-    return worldManager.getTerrainCollisionPoint(pos, pos.add(new Vector3f(0, -2.0f, 0)), 0.0f) != null;
+    
   }
   
   private void updateWorldMesh() {
@@ -323,19 +242,19 @@ public class ChunkRenderer extends SimpleApplication {
     // move character based on used input
     Vector3f oldPos = characterPos.clone();
     final float walkSpeed = 10;
-    if (activeInputs.contains(InputActions.WALK_FORWARD)) {
+    if (inputHandler.getActiveInputs().contains(InputActions.WALK_FORWARD)) {
       characterPos.z += FastMath.cos(character.getLookAzimuth()) * tpf * walkSpeed;
       characterPos.x += FastMath.sin(character.getLookAzimuth()) * tpf * walkSpeed;
     }
-    if (activeInputs.contains(InputActions.WALK_BACKWARD)) {
+    if (inputHandler.getActiveInputs().contains(InputActions.WALK_BACKWARD)) {
       characterPos.z -= FastMath.cos(character.getLookAzimuth()) * tpf * walkSpeed;
       characterPos.x -= FastMath.sin(character.getLookAzimuth()) * tpf * walkSpeed;
     }
-    if (activeInputs.contains(InputActions.STRAFE_LEFT)) {
+    if (inputHandler.getActiveInputs().contains(InputActions.STRAFE_LEFT)) {
       characterPos.z -= FastMath.sin(character.getLookAzimuth()) * tpf * walkSpeed;
       characterPos.x -= -FastMath.cos(character.getLookAzimuth()) * tpf * walkSpeed;
     }
-    if (activeInputs.contains(InputActions.STRAFE_RIGHT)) {
+    if (inputHandler.getActiveInputs().contains(InputActions.STRAFE_RIGHT)) {
       characterPos.z += FastMath.sin(character.getLookAzimuth()) * tpf * walkSpeed;
       characterPos.x += -FastMath.cos(character.getLookAzimuth()) * tpf * walkSpeed;
     }
@@ -394,4 +313,16 @@ public class ChunkRenderer extends SimpleApplication {
   public void setCloseEventHandler(CloseEventHandler closeEventHandler) {
     this.closeEventHandler = closeEventHandler;
   }
+
+  public void toggleFullScreen() {
+    isFullScreen = !isFullScreen;
+    setDisplayMode();
+    restart();
+  }
+
+  public void toggleFlyCam() {
+    flyCam.setEnabled(!flyCam.isEnabled());
+    inputManager.setCursorVisible(false);
+  }
+  
 }
---
+++ haju3d-client/src/main/java/fi/haju/haju3d/client/ui/input/WorldInputHandler.java
@@ -0,0 +1,115 @@
+package fi.haju.haju3d.client.ui.input;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseAxisTrigger;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+
+import fi.haju.haju3d.client.Character;
+import fi.haju.haju3d.client.ui.ChunkRenderer;
+import fi.haju.haju3d.client.ui.WorldManager;
+
+public class WorldInputHandler {
+  
+  public static final float MOUSE_X_SPEED = 3.0f;
+  public static final float MOUSE_Y_SPEED = MOUSE_X_SPEED;
+  
+  private final Character character;
+  private final WorldManager worldManager;
+  private final ChunkRenderer renderer;
+  private final Set<String> activeInputs = new HashSet<>();
+  
+  public WorldInputHandler(Character character, WorldManager worldManager, ChunkRenderer renderer) {
+    this.character = character;
+    this.worldManager = worldManager;
+    this.renderer = renderer;
+  }
+
+  public void register(final InputManager inputManager) {
+    // moving
+    inputManager.addMapping(InputActions.STRAFE_LEFT, new KeyTrigger(KeyInput.KEY_A));
+    inputManager.addMapping(InputActions.STRAFE_RIGHT, new KeyTrigger(KeyInput.KEY_D));
+    inputManager.addMapping(InputActions.WALK_FORWARD, new KeyTrigger(KeyInput.KEY_W));
+    inputManager.addMapping(InputActions.WALK_BACKWARD, new KeyTrigger(KeyInput.KEY_S));
+    inputManager.addListener(new ActionListener() {
+      @Override
+      public void onAction(String name, boolean isPressed, float tpf) {
+        if (isPressed) {
+          activeInputs.add(name);
+        } else {
+          activeInputs.remove(name);
+        }
+      }
+    }, InputActions.STRAFE_LEFT, InputActions.STRAFE_RIGHT, InputActions.WALK_FORWARD, InputActions.WALK_BACKWARD);
+
+    // looking
+    inputManager.addMapping(InputActions.LOOK_LEFT, new MouseAxisTrigger(MouseInput.AXIS_X, true), new KeyTrigger(KeyInput.KEY_LEFT));
+    inputManager.addMapping(InputActions.LOOK_RIGHT, new MouseAxisTrigger(MouseInput.AXIS_X, false), new KeyTrigger(KeyInput.KEY_RIGHT));
+    inputManager.addMapping(InputActions.LOOK_UP, new MouseAxisTrigger(MouseInput.AXIS_Y, false), new KeyTrigger(KeyInput.KEY_UP));
+    inputManager.addMapping(InputActions.LOOK_DOWN, new MouseAxisTrigger(MouseInput.AXIS_Y, true), new KeyTrigger(KeyInput.KEY_DOWN));
+
+    inputManager.addListener(new AnalogListener() {
+      @Override
+      public void onAnalog(String name, float value, float tpf) {
+        if (name.equals(InputActions.LOOK_LEFT)) {
+          character.setLookAzimuth(character.getLookAzimuth() + value * MOUSE_X_SPEED);
+        } else if (name.equals(InputActions.LOOK_RIGHT)) {
+          character.setLookAzimuth(character.getLookAzimuth() - value * MOUSE_X_SPEED);
+        } else if (name.equals(InputActions.LOOK_UP)) {
+          character.setLookElevation(character.getLookElevation() - value * MOUSE_Y_SPEED);
+        } else if (name.equals(InputActions.LOOK_DOWN)) {
+          character.setLookElevation(character.getLookElevation() + value * MOUSE_Y_SPEED);
+        }
+      }
+    }, InputActions.LOOK_LEFT, InputActions.LOOK_RIGHT, InputActions.LOOK_UP, InputActions.LOOK_DOWN);
+
+    // jumping
+    inputManager.addMapping(InputActions.JUMP, new KeyTrigger(KeyInput.KEY_SPACE));
+    inputManager.addListener(new ActionListener() {
+      @Override
+      public void onAction(String name, boolean isPressed, float tpf) {
+        if (isPressed && canJump(character.getNode())) {
+          character.setVelocity(character.getVelocity().add(new Vector3f(0.0f, 0.5f, 0.0f)));
+        }
+      }
+    }, InputActions.JUMP);
+
+    // toggle flycam
+    inputManager.addMapping(InputActions.TOGGLE_FLYCAM, new KeyTrigger(KeyInput.KEY_RETURN));
+    inputManager.addListener(new ActionListener() {
+      @Override
+      public void onAction(String name, boolean isPressed, float tpf) {
+        if (isPressed) {
+          renderer.toggleFlyCam();
+        }
+      }
+    }, InputActions.TOGGLE_FLYCAM);
+
+    // toggle fullscreen
+    inputManager.addMapping(InputActions.CHANGE_FULL_SCREEN, new KeyTrigger(KeyInput.KEY_F));
+    inputManager.addListener(new ActionListener() {
+      @Override
+      public void onAction(String name, boolean keyPressed, float tpf) {
+        renderer.toggleFullScreen();
+      }
+    }, InputActions.CHANGE_FULL_SCREEN);
+  }
+  
+  private boolean canJump(Node characterNode) {
+    Vector3f pos = characterNode.getLocalTranslation();
+    return worldManager.getTerrainCollisionPoint(pos, pos.add(new Vector3f(0, -2.0f, 0)), 0.0f) != null;
+  }
+
+  public Set<String> getActiveInputs() {
+    return activeInputs;
+  }
+  
+}
