50f9f79728f31f92e42540bd61e8d63f8c91fcb3
jvmakine <jvmakine@gmail.com>
2013-10-06 20:20:58 +0300

    TilePosition to store tile and chunk indexes

---
+++ haju3d-client/src/main/java/fi/haju/haju3d/client/TilePosition.java
@@ -0,0 +1,37 @@
+package fi.haju.haju3d.client;
+
+import com.jme3.math.Vector3f;
+
+import fi.haju.haju3d.protocol.Vector3i;
+
+public class TilePosition {
+  private final Vector3i chunkPosition;
+  private final Vector3i tileWithinChunk;
+  
+  public TilePosition(Vector3i chunkPosition, Vector3i tileWithinChunk) {
+    this.chunkPosition = chunkPosition;
+    this.tileWithinChunk = tileWithinChunk;
+  }
+
+  public Vector3i getChunkPosition() {
+    return chunkPosition;
+  }
+  
+  public Vector3i getTileWithinChunk() {
+    return tileWithinChunk;
+  }  
+  
+  public Vector3f getWorldPosition(float scale, int chunkSize) {
+    return new Vector3f(
+      (chunkPosition.x * chunkSize + tileWithinChunk.x) * scale + scale/2,
+      (chunkPosition.y * chunkSize + tileWithinChunk.y) * scale + scale/2,
+      (chunkPosition.z * chunkSize + tileWithinChunk.z) * scale + scale/2
+    );
+  }
+
+  @Override
+  public String toString() {
+    return "Chunk: " + chunkPosition + ", tile: " + tileWithinChunk;
+  }
+  
+}
