4e095cab1005124a9fe57e6ae7a3707aeb176ba8
jvmakine <jvmakine@gmail.com>
2013-10-17 23:32:43 +0300

    Less fog

--- haju3d-client/src/main/java/fi/haju/haju3d/client/ui/ChunkRenderer.java
+++ haju3d-client/src/main/java/fi/haju/haju3d/client/ui/ChunkRenderer.java
@@ -251,7 +251,7 @@ public class ChunkRenderer extends SimpleApplication {
     water.setRefractionStrength(0.1f);
     fpp.addFilter(water);
 
-    FogFilter fog = new FogFilter(new ColorRGBA(0.8f, 0.8f, 1.0f, 1.0f), 0.6f, 100.0f);
+    FogFilter fog = new FogFilter(new ColorRGBA(0.8f, 0.8f, 1.0f, 1.0f), 0.6f, 200.0f);
     fpp.addFilter(fog);
     viewPort.addProcessor(fpp);
   }
