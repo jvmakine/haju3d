60857172732bbfbb20082b23b5675d1488ffd1dc
Hannu Kankaanpaa <hannu.kp@gmail.com>
2013-12-20 17:06:34 +0200

    CharacterBoneApp: implemented symmetrical editing mode

--- haju3d-client/src/test/java/fi/haju/haju3d/client/CharacterBoneApp.java
+++ haju3d-client/src/test/java/fi/haju/haju3d/client/CharacterBoneApp.java
@@ -28,13 +28,7 @@ import static fi.haju.haju3d.client.SimpleApplicationUtils.makeLineMaterial;
 
 /**
  * TODO:
- * - RMB to create, LMB to edit
- * X LMB on empty to rotate
- * - symmetrical editing (forced?)
- * - create bone endpoint inside mesh, not on surface (try to snap to a "good" location..)
- * - the bigger bone is, the deeper inside its endpoint needs to be; how to make it so that endpoint location
- * is fixed regardless of bone size?
- * - snap dragPlane to x/y/z axes
+ * - ability to delete nodes
  * <p/>
  * Backlog:
  * - save/load bones
@@ -47,6 +41,19 @@ import static fi.haju.haju3d.client.SimpleApplicationUtils.makeLineMaterial;
  * - IK animation on bones
  * - quick way to create a leg or an arm (both consist of 3 bones)
  * - texturing
+ *
+ * Bone snapping:
+ * - create bone endpoint inside mesh, not on surface (try to snap to a "good" location..)
+ * - the bigger bone is, the deeper inside its endpoint needs to be; how to make it so that endpoint location
+ * is fixed regardless of bone size?
+ * - or maybe endpoint should be on surface, but bone mesh extends beyond endpoint?
+ * - maybe endpoint should always be forced on surface, no free movement allowed?
+ *
+ * Done
+ * - RMB to create, LMB to edit
+ * - symmetrical editing (forced?)
+ * - LMB on empty to rotate
+ * - snap dragPlane to x/y/z axes
  */
 public class CharacterBoneApp extends SimpleApplication {
 
@@ -62,6 +69,7 @@ public class CharacterBoneApp extends SimpleApplication {
     public static final String ROTATE_UP_MOUSE = "RotateUpMouse";
     public static final String ROTATE_DOWN_MOUSE = "RotateDownMouse";
     public static final String CLICK = "Click";
+    public static final String CLICK_RMB = "ClickRmb";
     public static final String RESIZE_DOWN = "ResizeDown";
     public static final String RESIZE_UP = "ResizeUp";
     public static final String SHOW_GUIDES = "ShowGuides";
@@ -74,6 +82,70 @@ public class CharacterBoneApp extends SimpleApplication {
     private float rotation;
     private Spatial spatial;
     private Spatial guiSpatial;
+    private MyBone mirrorBone;
+
+    public void setMirrorBone(MyBone mirrorBone) {
+      // detach from old mirror:
+      if (this.mirrorBone != null) {
+        this.mirrorBone.mirrorBone = null;
+      }
+      // set up new mirror link:
+      this.mirrorBone = mirrorBone;
+      if (mirrorBone != null) {
+        mirrorBone.mirrorBone = this;
+      }
+    }
+
+    public void addThickness(float value) {
+      addThicknessSelf(value);
+      if (mirrorBone != null) {
+        mirrorBone.addThicknessSelf(value);
+      }
+    }
+
+    public void addThicknessSelf(float value) {
+      this.thickness += value * 0.05f;
+      if (this.thickness < MINIMUM_BONE_THICKNESS) {
+        this.thickness = MINIMUM_BONE_THICKNESS;
+      }
+    }
+
+    public void setPosition(Vector3f p, boolean start) {
+      setPositionSelf(p, start);
+      if (mirrorBone != null) {
+        mirrorBone.setPositionSelf(getMirroredVector(p), start);
+      }
+    }
+
+    public void setPositionSelf(Vector3f p, boolean start) {
+      if (start) {
+        this.start = p;
+      } else {
+        this.end = p;
+      }
+    }
+  }
+
+  private static class DragTarget {
+    private MyBone bone;
+    private boolean isStart;
+
+    private DragTarget(MyBone bone, boolean isStart) {
+      this.bone = bone;
+      this.isStart = isStart;
+    }
+
+    public Vector3f getPosition() {
+      return isStart ? bone.start : bone.end;
+    }
+
+    public void setPosition(Vector3f p) {
+      bone.setPosition(p, isStart);
+    }
+  }
+
+  public static Vector3f getMirroredVector(Vector3f p) {
+    return new Vector3f(-p.x, p.y, p.z);
   }
 
   public static void main(String[] args) {
@@ -89,7 +161,7 @@ public class CharacterBoneApp extends SimpleApplication {
   private float camAzimuth = 0;
   private List<MyBone> bones = new ArrayList<>();
 
-  private Vector3f dragTarget;
+  private DragTarget dragTarget;
   private Spatial dragPlane;
   private Spatial dragPlanePreview;
   private boolean showGuides = true;
@@ -180,25 +252,22 @@ public class CharacterBoneApp extends SimpleApplication {
         }
         MyBone bone = findCurrentBone();
         if (bone != null) {
-          bone.thickness += value * 0.05f;
-          if (bone.thickness < MINIMUM_BONE_THICKNESS) {
-            bone.thickness = MINIMUM_BONE_THICKNESS;
-          }
+          bone.addThickness(value);
         }
       }
     }, Actions.RESIZE_DOWN, Actions.RESIZE_UP);
 
     inputManager.addMapping(Actions.CLICK, new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+    inputManager.addMapping(Actions.CLICK_RMB, new MouseButtonTrigger(MouseInput.BUTTON_RIGHT));
     inputManager.addListener(new ActionListener() {
       @Override
       public void onAction(String name, boolean isPressed, float tpf) {
         if (isPressed) {
           stopDragging();
-          dragTarget = findDragTarget();
-          if (dragTarget != null) {
-            startDragging();
+          if (name.equals(Actions.CLICK)) {
+            dragTarget = findDragTarget();
           } else {
-            Vector3f attachPoint = findBoneAttachPoint();
+            Vector3f attachPoint = findBoneCollisionPoint();
             if (attachPoint != null) {
               // create new bone
               MyBone bone = new MyBone();
@@ -207,19 +276,31 @@ public class CharacterBoneApp extends SimpleApplication {
               bone.thickness = 0.2f;
               bone.spatial = makeSphere();
               bones.add(bone);
-              dragTarget = bone.end;
+              dragTarget = new DragTarget(bone, false);
+
+              MyBone bone2 = new MyBone();
+              bone2.start = getMirroredVector(attachPoint);
+              bone2.end = getMirroredVector(attachPoint);
+              bone2.thickness = 0.2f;
+              bone2.spatial = makeSphere();
+              bones.add(bone2);
+
+              bone.setMirrorBone(bone2);
               startDragging();
-            } else {
-              // start moving camera
-              cameraDragging = true;
             }
+          }
+          if (dragTarget != null) {
+            startDragging();
+          } else {
+            // start moving camera
+            cameraDragging = true;
           }
         } else {
           stopDragging();
           cameraDragging = false;
         }
       }
-    }, Actions.CLICK);
+    }, Actions.CLICK, Actions.CLICK_RMB);
 
     inputManager.addMapping(Actions.SHOW_GUIDES, new KeyTrigger(KeyInput.KEY_SPACE));
     inputManager.addListener(new ActionListener() {
@@ -245,12 +326,28 @@ public class CharacterBoneApp extends SimpleApplication {
     if (dragPlanePreview != null) {
       rootNode.detachChild(dragPlanePreview);
     }
+    // possibly remove mirror buddy if bones lie along X plane
+    if (dragTarget != null && dragTarget.bone.mirrorBone != null) {
+      final float snapToXDistance = 0.3f;
+      if (FastMath.abs(dragTarget.bone.start.x) < snapToXDistance && FastMath.abs(dragTarget.bone.end.x) < snapToXDistance) {
+        removeBone(dragTarget.bone.mirrorBone);
+        dragTarget.bone.start.x = 0;
+        dragTarget.bone.end.x = 0;
+      }
+    }
     dragPlane = null;
     dragPlanePreview = null;
     dragTarget = null;
   }
 
-  private Vector3f findBoneAttachPoint() {
+  public void removeBone(MyBone bone) {
+    bone.setMirrorBone(null);
+    rootNode.detachChild(bone.spatial);
+    guiNode.detachChild(bone.guiSpatial);
+    bones.remove(bone);
+  }
+
+  private Vector3f findBoneCollisionPoint() {
     CollisionResult collision = findBoneCollision();
     if (collision != null) {
       return collision.getContactPoint();
@@ -284,19 +381,19 @@ public class CharacterBoneApp extends SimpleApplication {
     return null;
   }
 
-  private Vector3f findDragTarget() {
-    Vector3f best = null;
+  private DragTarget findDragTarget() {
+    DragTarget best = null;
     float bestDist = 20;
     for (MyBone bone : bones) {
       float distance = cursorDistanceTo(bone.start);
       if (distance < bestDist) {
         bestDist = distance;
-        best = bone.start;
+        best = new DragTarget(bone, true);
       }
       distance = cursorDistanceTo(bone.end);
       if (distance < bestDist) {
         bestDist = distance;
-        best = bone.end;
+        best = new DragTarget(bone, false);
       }
     }
     return best;
@@ -333,7 +430,14 @@ public class CharacterBoneApp extends SimpleApplication {
     // mesh should be a plane [x=[0..sz],y=[0..sz],z=0]
     Vector3f direction = getSnappedVector(cam.getDirection());
     Vector3f left = getSnappedVector(cam.getLeft());
-    Vector3f center = dragTarget;
+    Vector3f center = dragTarget.getPosition();
+
+    // non-mirrored bones only dragged along X-plane
+    if (dragTarget.bone.mirrorBone == null) {
+      direction = Vector3f.UNIT_X;
+      left = Vector3f.UNIT_Z;
+    }
+
     return makeDragPlane(sz, mesh, material, direction, left, center);
   }
 
@@ -368,7 +472,7 @@ public class CharacterBoneApp extends SimpleApplication {
       Ray ray = getCursorRay();
       CollisionResults results = new CollisionResults();
       if (dragPlane.collideWith(ray, results) > 0) {
-        dragTarget.set(results.getCollision(0).getContactPoint());
+        dragTarget.setPosition(results.getCollision(0).getContactPoint());
       }
     }
 
@@ -476,7 +580,7 @@ public class CharacterBoneApp extends SimpleApplication {
   }
 
   public Spatial makeSphere() {
-    Mesh m = new Sphere(20, 20, 0.6f);
+    Mesh m = new Sphere(20, 20, 0.7f);
     final Geometry geom = new Geometry("ColoredMesh", m);
     geom.setMaterial(makeColorMaterial(assetManager, ColorRGBA.White));
     geom.setShadowMode(RenderQueue.ShadowMode.CastAndReceive);
